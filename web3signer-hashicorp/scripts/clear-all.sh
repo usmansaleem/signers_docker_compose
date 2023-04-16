fb8b96ca9c842bea93a5d7d9c740bffe86557686
Usman Saleem <usman@usmans.info>
2023-04-16 12:09:16 +1000

    Change docker compose container names

--- web3signer-hashicorp/scripts/clear-all.sh
+++ web3signer-hashicorp/scripts/clear-all.sh
@@ -15,12 +15,12 @@ docker compose down --rmi all
 popd
 
 echo "Vault: Removing data ..."
-rm -rf $SCRIPT_DIR/../vault/data/*
+rm -rf $SCRIPT_DIR/../vault/data/.
 echo "Vault: Removing certs ..."
-rm -rf $SCRIPT_DIR/../vault/certs/*
+rm -rf $SCRIPT_DIR/../vault/certs/.
 echo "Vault: Removing creds ..."
-rm -rf $SCRIPT_DIR/../vault/creds/*
+rm -rf $SCRIPT_DIR/../vault/creds/.
 echo "Web3Signer: Removing configuration files ..."
-rm -rf $SCRIPT_DIR/../web3signer/config/keys/*
+rm -rf $SCRIPT_DIR/../web3signer/config/keys/
 echo "Web3Signer: Removing knownhosts file ..."
 rm -fv $SCRIPT_DIR/../web3signer/config/knownhosts
